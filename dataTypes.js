397421532b629f7972182c0adb6993d882e76aeb
Pavel <122470727+Skat-58@users.noreply.github.com>
2023-05-10 00:35:13 +0300

    basics JavaScript

---
+++ dataTypes.js
@@ -0,0 +1,11 @@
+// Шаблонные строки
+
+// Что выведет этот скрипт?
+
+let name = "Ilya";
+
+alert(`hello ${1}`); //  hello 1            - обратные ковычки с выражением 1
+
+alert(`hello ${"name"}`); // hello name     - с выражением строки "name"
+
+alert(`hello ${name}`); // hello Ilya       - c переменной которую мы задали
